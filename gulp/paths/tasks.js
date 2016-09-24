f7d69d6226e26eb05fb2dd57d98037bd019f28f3
Artem Boyur <gboyur@gmail.com>
2016-09-24 13:58:41 +0300

    DZ2 add fonts, merge task copy

--- gulp/paths/tasks.js
+++ gulp/paths/tasks.js
@@ -10,9 +10,8 @@ module.exports = [
   './gulp/tasks/css.foundation.js',
   './gulp/tasks/js.process.js',
   './gulp/tasks/js.lint.js',
-  './gulp/tasks/copy.image.js',
+  './gulp/tasks/copy.js',
   './gulp/tasks/sprite.svg.js',
-  './gulp/tasks/sprite.img.js',
-  './gulp/tasks/copy.sprite.js'
+  './gulp/tasks/sprite.img.js'
 ];
 
--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,6 +1,11 @@
 'use strict';
 
 module.exports = function() {
+  $.gulp.task('copy:image', function() {
+    return $.gulp.src(['./source/images/**/*.*','!./source/images/sprite/**/*.*'], { since: $.gulp.lastRun('copy:image') })
+      .pipe($.gulp.dest($.config.root + '/assets/img'));
+  });
+
   $.gulp.task('copy:sprite', function() {
     return $.gulp.src('./source/images/sprite/img/*.png', { since: $.gulp.lastRun('copy:sprite') })
       .pipe($.gulp.dest($.config.root + '/assets/img/sprite'));
