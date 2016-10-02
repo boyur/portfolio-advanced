86929783b2c8ce349508f841af861aff96cd9fa7
Artem Boyur <gboyur@gmail.com>
2016-10-02 12:25:39 +0300

    all pages ready

--- source/js/app.js
+++ source/js/app.js
@@ -1,35 +1,59 @@
-(function() {
-  'use strict';
+window.onload = function () {
+  (function () {
+    'use strict';
 
-  auth_btn.addEventListener("click", showAuth);
-  welcome_btn.addEventListener("click", showWelcome);
+    if (document.getElementById('auth_btn') || document.getElementById('welcome_btn')) {
+      auth_btn.addEventListener("click", showAuth);
+      welcome_btn.addEventListener("click", showWelcome);
+    }
 
-  function showAuth() {
-    document.getElementById('welcome_block' ).style.transform = 'scaleX(0)';
-    // Я сделяль
-    setTimeout(function () {
-      document.getElementById('welcome_block' ).style.display = 'none';
-      document.getElementById('auth_block' ).style.display = 'inline-block';
+    function showAuth() {
+      document.getElementById('welcome_block').style.transform = 'scaleX(0)';
+      // Я сделяль
+      setTimeout(function () {
+        document.getElementById('welcome_block').style.display = 'none';
+        document.getElementById('auth_block').style.display = 'inline-block';
+
+        setTimeout(function () {
+          document.getElementById('auth_block').style.transform = 'scaleX(1)';
+        }, 10);
+
+      }, 600);
+    }
+
+    function showWelcome() {
+      document.getElementById('auth_block').style.transform = 'scaleX(0)';
 
       setTimeout(function () {
-        document.getElementById('auth_block' ).style.transform = 'scaleX(1)';
-      },10);
+        document.getElementById('auth_block').style.display = 'none';
+        document.getElementById('welcome_block').style.display = 'inline-block';
 
-    },600);
-  }
+        setTimeout(function () {
+          document.getElementById('welcome_block').style.transform = 'scaleX(1)';
+        }, 10);
 
-  function showWelcome() {
-    document.getElementById('auth_block' ).style.transform = 'scaleX(0)';
+      }, 600);
+    }
 
-    setTimeout(function () {
-      document.getElementById('auth_block' ).style.display = 'none';
-      document.getElementById('welcome_block' ).style.display = 'inline-block';
+  })();
 
-      setTimeout(function () {
-        document.getElementById('welcome_block' ).style.transform = 'scaleX(1)';
-      },10);
+// Yandex map
+  (function () {
 
-    },600);
-  }
+    ymaps.ready(init);
+    var
+      myMap,
+      myPlacemark;
 
-})();
+    function init() {
+      myMap = new ymaps.Map("map", {
+        center: [44.73498924390664, 37.73530074198924],
+        zoom: 14,
+        controls: []
+      });
+
+      myMap.behaviors.disable('scrollZoom');
+    }
+
+  })();
+}();
