3fd081c96df136f9a069afea058f038a869f1ab4
Artem Boyur <gboyur@gmail.com>
2016-10-04 19:46:13 +0300

    PostCSS migration ready, add adaptive start page

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,71 @@
 'use strict';
 
-global.$ = {
-  package: require('./package.json'),
-  config: require('./gulp/config'),
-  path: {
-    task: require('./gulp/paths/tasks.js'),
-    jsFoundation: require('./gulp/paths/js.foundation.js'),
-    cssFoundation: require('./gulp/paths/css.foundation.js'),
-    app: require('./gulp/paths/app.js')
-  },
-  gulp: require('gulp'),
-  rimraf: require('rimraf'),
-  browserSync: require('browser-sync').create(),
-  gp: require('gulp-load-plugins')()
-};
 
-$.path.task.forEach(function(taskPath) {
-  require(taskPath)();
+const gulp = require('gulp');
+//const gulpCopy = require('gulp-file-copy');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const assets = require('postcss-assets');
+const autoprefixer = require('autoprefixer');
+const concat = require('gulp-concat');
+//const concatCss = require('gulp-concat-css');
+const reporter = require('postcss-browser-reporter');
+const nested = require('postcss-nested');
+const short = require('postcss-short');
+const atImport = require("postcss-import");
+const customMedia = require("postcss-custom-media")
+
+const stylelint = require('stylelint');
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+
+//const rulesStyles = require('./stylelintrc.json');
+
+// Pug
+gulp.task('pug', function buildHTML() {
+  return gulp.src('./source/template/pages/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./build'));
 });
 
-$.gulp.task('default', $.gulp.series(
-  'clean',
-  $.gulp.parallel(
-    'sass',
-    'pug',
-    'js:foundation',
-    'js:process',
-    'copy:image',
-    'copy:fonts',
-    'css:foundation',
-    'sprite:svg'
-  ),
-  $.gulp.parallel(
-    'watch',
-    'serve'
-  )
-));
+// PostCSS
+gulp.task('styles', function() {
+  const processors = [
+    atImport,
+    nested,
+    assets,
+    short,
+    customMedia,
+    //stylelint(rulesStyles),
+    autoprefixer,
+    reporter({
+      selector: 'body:before'
+    })
+  ];
+
+  return gulp.src('./source/style/*.pcss')
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(concat('style.min.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./build/assets/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('source/style/**/*.pcss', gulp.series('styles'));
+  gulp.watch('source/template/pages/*.pug', gulp.series('pug'));
+});
+
+// Запуска сервера
+gulp.task('browser-sync', function() {
+
+  gulp.watch('build/assets/**/*.*').on('change', browserSync.reload);
+
+  return browserSync.init({
+    server: {
+      baseDir: './build'
+    }
+  });
+});
